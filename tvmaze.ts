5d08fa9cfb742ab8d455f010bf3c44c4ef41eb71
Jessica <jessica@Jessicas-MacBook-Pro.local>
2021-04-20 17:34:42 -0700

    add functions to add shows and episodes to webpage

--- tvmaze.ts
+++ tvmaze.ts
@@ -4,6 +4,9 @@ import * as $ from 'jquery';
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const $searchFormTerm = $("#searchForm-term");
+const $episodesBtn = $(".Show-getEpisodes");
+const BASE_URL = "http://api.tvmaze.com"
 
 
 /** Given a search term, search for tv shows that match that query.
@@ -13,45 +16,31 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
+async function getShowsByTerm(term:string) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
-}
+  let resp = await axios.get(`${BASE_URL}/search/shows?q=${term}`)
 
+  let arrayOfShows = resp.data
+
+  return arrayOfShows;
+}
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows) {
+function populateShows(shows: Array<any>) {
   $showsList.empty();
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+        `<div data-show-id="${show.show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src=${show.show.image.medium || "https://tinyurl.com/tv-missing"}
+              alt="${show.show.name}"
               class="w-25 mr-3">
            <div class="media-body">
-             <h5 class="text-primary">${show.name}</h5>
-             <div><small>${show.summary}</small></div>
+             <h5 class="text-primary">${show.show.name}</h5>
+             <div><small>${show.show.summary}</small></div>
              <button class="btn btn-outline-light btn-sm Show-getEpisodes">
                Episodes
              </button>
@@ -69,7 +58,7 @@ function populateShows(shows) {
  */
 
 async function searchForShowAndDisplay() {
-  const term = $("#searchForm-term").val();
+  const term = $searchFormTerm.val();
   const shows = await getShowsByTerm(term);
 
   $episodesArea.hide();
@@ -82,12 +71,43 @@ $searchForm.on("submit", async function (evt) {
 });
 
 
+
+
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id:number) {
+  let resp = await axios.get(`${BASE_URL}/shows/${id}/episodes`)
+
+  let arrayOfEps = resp.data
+  console.log(arrayOfEps)
+  return arrayOfEps;
+ }
 
 /** Write a clear docstring for this function... */
 
-// function populateEpisodes(episodes) { }
+function populateEpisodes(episodes: Array<any>) { 
+
+
+  for (let episode of episodes) {
+
+    const $ep = $(
+      `<ul>
+      <li>${episode.name}</li>
+      <li>${episode.summary}</li>
+      </ul>
+      `);
+
+    $episodesArea.append($ep);  }
+}
+
+
+$showsList.on("click", async function (evt) {
+
+  $episodesArea.css("display", "block")
+  let show = evt.target.parentElement.parentElement.parentElement
+  let showId = show.getAttribute("data-show-id")
+  let eps = await getEpisodesOfShow(parseInt(showId));
+  populateEpisodes(eps)
+})
